8c2f154f75ea615577528f7e23a56569876dc500
Jan Heidbrink <jan.heidbrink@tngtech.com>
2022-12-02 16:43:31 +0100

    Functional approach for day 1

--- day1/most_calories.py
+++ day1/most_calories.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python
 
+from itertools import groupby
+from typing import Optional
+
 calory = int
 foods = list[calory]
 
+def parse_line(line: str) -> Optional[calory]:
+    try:
+        return int(line.strip())
+    except ValueError:
+        return None
 
-def read_elves() -> list[foods]:
-    with open('day1_input', encoding='utf-8') as fh:
-        contents = fh.read()
-    elves: list[foods] = []
-    current_elf: foods = []
-    for line in contents.splitlines():
-        if not line.strip():
-            elves.append(current_elf)
-            current_elf = []
-        else:
-            current_elf.append(int(line.strip()))
-    return elves
+def readfile(filename: str) -> str:
+    with open(filename, encoding='utf8') as fh:
+        return fh.read()
 
+def parse_input(contents: str) -> list[foods]:
+    """
+    >>> parse_input('10\\n20\\n\\n100')
+    [[10, 20], [100]]
+    """
+    values: list[Optional[int]] = [parse_line(line) for line in contents.splitlines()]
+    return [list(group) for key, group in groupby(values, lambda value: value is None) if not key]
 
 def elf_with_most_calories(elves: list[foods]) -> int:
     return max([sum(all_foods) for all_foods in elves])
@@ -27,10 +33,9 @@ def calories_carried_by_top_three_elves(elves: list[foods]) -> int:
 
 
 def main():
-    elves = read_elves()
-    most_calories = elf_with_most_calories(elves)
-    print(most_calories)
-    print(calories_carried_by_top_three_elves(elves))
+    elves: list[foods] = parse_input(readfile('day1_input'))
+    print(f"most calories: {elf_with_most_calories(elves)}")
+    print(f"calories carried by top 3 elves: {calories_carried_by_top_three_elves(elves)}")
 
 
 if __name__ == "__main__":
