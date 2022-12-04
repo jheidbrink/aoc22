156803d87aa31d1a47fbef47bc49cf24979f5c6c
Jan Heidbrink <jan.heidbrink@tngtech.com>
2022-12-04 09:52:19 +0100

    Day 4 part 2 / Python

--- day04/day04.py
+++ day04/day04.py
@@ -10,20 +10,30 @@ class Section:
         self.lower = lower
         self.upper = upper
 
-    def __contains__(self, other: "Section"):
+    def __contains__(self, other: "Section") -> bool:
         return other.lower >= self.lower and other.upper <= self.upper
+
+    def partially_in(self, other: "Section") -> bool:
+        return (other.lower <= self.lower <= other.upper) or (other.lower <= self.upper <= other.upper)
 
     @staticmethod
     def parse(input_: str) -> "Section":
         lower, upper = input_.split('-')
         return Section(int(lower), int(upper))
 
-def count_overlaps(input_lines: list[str]) -> int:
-    return len([1 for line in input_lines if sections_overlap(line)])
+def count_complete_overlaps(input_lines: list[str]) -> int:
+    return len([1 for line in input_lines if sections_overlap_completely(line)])
 
-def sections_overlap(line: str) -> bool:
+def count_partial_overlaps(input_lines: list[str]) -> int:
+    return len([1 for line in input_lines if sections_overlap_partially(line)])
+
+def sections_overlap_completely(line: str) -> bool:
     left, right = parse_line(line)
     return (left in right) or (right in left)
+
+def sections_overlap_partially(line: str) -> bool:
+    left, right = parse_line(line)
+    return left.partially_in(right) or right.partially_in(left)
 
 def parse_line(line: str) -> tuple[Section, Section]:
     left, right = line.split(',')
@@ -32,8 +42,8 @@ def parse_line(line: str) -> tuple[Section, Section]:
 def main():
     input_lines = read_input().splitlines()
     data = {
-        'part 1': count_overlaps(input_lines),
-        'part 2': None,
+        'part 1': count_complete_overlaps(input_lines),
+        'part 2': count_partial_overlaps(input_lines),
     }
     print(data)
 
